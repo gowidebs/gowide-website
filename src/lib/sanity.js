193441f5e3b15384f788f2ef1eab3970a9a667cc
gowidebs <your-gowidebs-email@example.com>
2025-09-24 18:37:44 +0400

    Fix Sanity API version and hardcode project config

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -1,10 +1,10 @@
 import { createClient } from '@sanity/client'
 
 export const client = createClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID || '5nkpumup',
-  dataset: process.env.REACT_APP_SANITY_DATASET || 'production',
+  projectId: '5nkpumup',
+  dataset: 'production',
   useCdn: true,
-  apiVersion: '2021-10-21',
+  apiVersion: '2023-05-03',
   token: process.env.REACT_APP_SANITY_TOKEN
 })
 
